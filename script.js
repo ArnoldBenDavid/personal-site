ff1969c18b494196ac807a6b8bb90aa6f50f3027
ArnoldBenDavid <103967841+ArnoldBenDavid@users.noreply.github.com>
2022-06-07 23:01:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+const circle = document.getElementById('circle');
+const circleStyle = circle.style;
+
+document.addEventListener('mousemove', e => {
+  window.requestAnimationFrame(() => {
+    circleStyle.top = `${ e.clientY - circle.offsetHeight/2 }px`;
+    circleStyle.left = `${ e.clientX - circle.offsetWidth/2 }px`;
+  });
+});
+
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
